c9b5611a11ba51058de0806df989b10fefa2168d
frednovack <fredynovack@gmail.com>
2017-03-02 19:43:26 -0300

    Initializing project

---
+++ Viva Decora/Viva Decora/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Viva Decora
+//
+//  Created by Frederico Novack Amaral Pereira on 3/2/17.
+//  Copyright © 2017 frednovack. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
